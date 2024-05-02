f731c692e11628e143c5f914908f09c6f90db0d6
moguism <mauriciorosso.aboy@gmail.com>
2024-05-02 11:17:42 +0200

    Agregada mi parte

--- gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/models/Mensualidad.java
+++ gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/models/Mensualidad.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.macauris.gestionComunitaria.models;
 
-/**
- *
- * @author moguism
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name="Mensualidades")
 public class Mensualidad {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idMensualidades;
+    private Date Fecha;
+
+    public Long getIdMensualidades() {
+        return idMensualidades;
+    }
+
+    public void setIdMensualidades(Long idMensualidades) {
+        this.idMensualidades = idMensualidades;
+    }
+
+    public Date getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(Date Fecha) {
+        this.Fecha = Fecha;
+    }
     
 }
