b58014eadb4196fc5a8c7b2cf15e912ace87f111
moguism <mauriciorosso.aboy@gmail.com>
2024-05-02 09:34:45 +0200

    Proyecto en Spring

---
+++ gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/GestionComunitariaApplication.java
@@ -0,0 +1,13 @@
+package com.macauris.gestionComunitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionComunitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionComunitariaApplication.class, args);
+	}
+
+}
