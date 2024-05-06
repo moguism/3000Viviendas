55a020adadb926fd94d6ef9422931139e947e58d
CarlosPerezValiente <pian1313cpv@digitechfp.com>
2024-05-06 12:57:10 +0200

    MONDONGO

--- gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/models/TipoDeudas.java
+++ gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/models/TipoDeudas.java
@@ -4,7 +4,7 @@
  */
 package com.macauris.gestionComunitaria.models;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
