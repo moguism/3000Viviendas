e09160ff5f334609ba07e54a8c983d964872b412
moguism <mauriciorosso.aboy@gmail.com>
2024-05-06 12:23:25 +0200

    Agregados varios repositorios

---
+++ gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/repositories/TipoReunionRepository.java
@@ -0,0 +1,8 @@
+package com.macauris.gestionComunitaria.repositories;
+
+import com.macauris.gestionComunitaria.models.TipoReunion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoReunionRepository extends JpaRepository<TipoReunion, Long> {
+    
+}
