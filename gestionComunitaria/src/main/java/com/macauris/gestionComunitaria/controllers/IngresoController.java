e2d9d6974b127576306984207f5134fde92568d7
unknown <pian1313cpv@digitechfp.com>
2024-05-16 11:05:45 +0200

    mondongo

--- gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/controllers/IngresoController.java
+++ gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/controllers/IngresoController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.macauris.gestionComunitaria.repositories.IngresoRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @CrossOrigin(origins = "*") // Permitir todos los orígenes
@@ -31,4 +36,41 @@ public class IngresoController {
         Optional<Ingreso> ingreso = ingresoRepository.findById(id);
         return ingreso.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+        @PostMapping // CUANDO RECIBA UNA PETICIÓN POR POST A LA URL EJECUTA ESTO
+    public ResponseEntity<Ingreso> createIngreso(@RequestBody Ingreso ingreso){ // "@RequestBody" LE DICE QUE VA A RECIBIR EL OBJETO A TRAVÉS DEL CUERPO DE LA PETICIÓN
+        Ingreso savedIngreso = ingresoRepository.save(ingreso);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedIngreso);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteIngreso(@PathVariable Long id){
+
+        if(!ingresoRepository.existsById(id)){
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+        ingresoRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Ingreso> updateIngreso(@PathVariable Long id, @RequestBody Ingreso ingreso){
+
+        if(!ingresoRepository.existsById(id)){
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+        ingreso.setId(id);
+        Ingreso savedIngreso = ingresoRepository.save(ingreso); // EN EL "save" HA ACTUALIZADO
+
+        return ResponseEntity.ok(savedIngreso);
+
+    }
+
 }
--- gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/controllers/MensualidadController.java
+++ gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/controllers/MensualidadController.java
@@ -8,9 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.macauris.gestionComunitaria.repositories.MensualidadRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @CrossOrigin(origins = "*") // Permitir todos los orígenes
@@ -32,4 +37,41 @@ public class MensualidadController {
         Optional<Mensualidad> mensualidad = mensualidadRepository.findById(id);
         return mensualidad.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+        @PostMapping // CUANDO RECIBA UNA PETICIÓN POR POST A LA URL EJECUTA ESTO
+    public ResponseEntity<Mensualidad> createMensualidad(@RequestBody Mensualidad mensualidad){ // "@RequestBody" LE DICE QUE VA A RECIBIR EL OBJETO A TRAVÉS DEL CUERPO DE LA PETICIÓN
+        Mensualidad savedMensualidad = mensualidadRepository.save(mensualidad);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedMensualidad);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMensualidad(@PathVariable Long id){
+
+        if(!mensualidadRepository.existsById(id)){
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+        mensualidadRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Mensualidad> updateMensualidad(@PathVariable Long id, @RequestBody Mensualidad mensualidad){
+
+        if(!mensualidadRepository.existsById(id)){
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+        mensualidad.setId(id);
+        Mensualidad savedMensualidad = mensualidadRepository.save(mensualidad); // EN EL "save" HA ACTUALIZADO
+
+        return ResponseEntity.ok(savedMensualidad);
+
+    }
+
 }
