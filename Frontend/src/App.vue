dea4e40383e7ea10462555564905be0668395740
moguism <mauriciorosso.aboy@gmail.com>
2024-05-17 19:17:58 +0200

    BOF

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
 <Header></Header>
 <Home></Home>
 </template>
@@ -6,4 +6,16 @@
 <script lang="ts" setup>
 import Header from './components/Header.vue'
 import Home from './components/Home.vue'
-</script>
+</script>-->
+<!-- src/App.vue -->
+<template>
+    <div id="app">
+      <router-view></router-view>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    name: 'App'
+  }
+  </script>
--- Frontend/src/router.ts
+++ Frontend/src/router.ts
@@ -1,18 +1,24 @@
-import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Comunidad from './components/Comunidad.vue';
-import Home from './components/Home.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './components/Home.vue'
+import Bloque from './components/Bloque.vue'
 
-Vue.use(VueRouter);
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/bloque/:id',
+    name: 'Bloque',
+    component: Bloque,
+    props: true
+  }
+]
 
-const routes: Array<RouteConfig> = [
-  { path: '/', component: Home },
-  { path: '/comunidad', component: Comunidad }
-];
-
-const router = new VueRouter({
-  mode: 'history',
+const router = createRouter({
+  history: createWebHistory('/'), // Puedes cambiar '/' por la base URL de tu aplicación si es necesario
   routes
-});
+})
 
-export default router;
+export default router
