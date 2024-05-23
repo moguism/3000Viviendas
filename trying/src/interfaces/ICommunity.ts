f148a2ea6c85b9e6217f8682f77c9db7c3d64bf1
Digirocu <ismaelrodcuenca@digitechfp.com>
2024-05-23 13:28:08 +0200

    Mauricio no me borres nada mas, gracias

--- trying/src/interfaces/ICommunity.ts
+++ trying/src/interfaces/ICommunity.ts
@@ -1,5 +1,7 @@
+import type IBloque from '../interfaces/IBloque.ts' 
 export default interface ICommunity {
     id: number,
     nombre: string,
-    direccion: string;
+    direccion: string,
+    bloques: Array<IBloque>;
 }
