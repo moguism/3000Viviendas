f9984994f4b330cf7b1823e48e7a56b357014429
Digirocu <ismaelrodcuenca@digitechfp.com>
2024-05-30 12:17:15 +0200

    Error super ultra mega super fuerte de Mauricio, maldito sea Mauricio

--- gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/models/Mensualidad.java
+++ gestionComunitaria/src/main/java/com/macauris/gestionComunitaria/models/Mensualidad.java
@@ -1,17 +1,14 @@
 package com.macauris.gestionComunitaria.models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.util.List;
 
